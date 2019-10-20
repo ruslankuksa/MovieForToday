1991c2ce52be73efe2a5bf686526ce2c657d5016
Ruslan <templar1128@hotmail.com>
2019-10-20 14:09:43 +0300

    Initial Commit

---
+++ MovieForToday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieForToday
+//
+//  Created by Руслан Кукса on 20.10.2019.
+//  Copyright © 2019 Руслан Кукса. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
